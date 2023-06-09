3ce71c6a98152417a8baeb40c3009594a9b25ae9
mlmlmlml00700700700 <128768493+mlmlmlml00700700700@users.noreply.github.com>
2023-06-09 21:11:17 +0900

    원래대로

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -5,7 +5,6 @@ const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Delicious Food, Deliverd To You</h2>
-      <img src="img/a2.jpg" alt="이미지" /> 
       <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>	
       <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!</p>
       
