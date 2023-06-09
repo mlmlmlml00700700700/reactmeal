647afafe035e6d3ea0f2726ae5a9caf40035c290
mlmlmlml00700700700 <128768493+mlmlmlml00700700700@users.noreply.github.com>
2023-06-09 20:48:01 +0900

    이미지사용연습

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -7,6 +7,7 @@ const MealsSummary = () => {
       <h2>Delicious Food, Deliverd To You</h2>
       <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>	
       <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!</p>
+      <img src="img/a2.jpg" alt="이미지" /> 
     </section>
   )
 }
